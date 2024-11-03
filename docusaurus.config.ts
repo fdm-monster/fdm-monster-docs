49be920b42ced1ae1026d65ea8613fa4438ecccb
Maurice Kevenaar <maurice@kevenaar.name>
2024-11-03 14:36:47 +0100

    (maint) docs site revamp

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,10 +1,14 @@
 import { themes as prismThemes } from "prism-react-renderer";
 import type { Config } from "@docusaurus/types";
-import type * as Preset from "@docusaurus/preset-classic";
+import type { Options as PresetOptions } from "@docusaurus/preset-classic";
+import type { Options as DocsOptions } from "@docusaurus/plugin-content-docs";
+import type { Options as BlogOptions } from "@docusaurus/plugin-content-blog";
+import type { Options as ClientRedirectsOptions } from "@docusaurus/plugin-client-redirects";
 
 const config: Config = {
   title: "FDM Monster Docs",
-  tagline: "3D printer farm management platform for local or in-cloud usage using OctoPrint.",
+  tagline:
+    "3D printer farm management platform for local or in-cloud usage using OctoPrint.",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
@@ -22,145 +26,205 @@ const config: Config = {
   trailingSlash: false,
 
   onBrokenLinks: "throw",
-  onBrokenAnchors: "warn",
-  onBrokenMarkdownLinks: "warn",
+  onBrokenAnchors: "throw",
+  onBrokenMarkdownLinks: "throw",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: "en",
-    locales: ["en"]
+    locales: ["en"],
   },
 
   presets: [
     [
-      "classic",
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: "./sidebars.ts",
+          path: "docs",
+          breadcrumbs: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/fdm-monster/fdm-monster-docs",
+          editUrl: ({ versionDocsDirPath, docPath }) =>
+            `https://github.com/fdm-monster/fdm-monster-docs/edit/main/${versionDocsDirPath}/${docPath}`,
+          sidebarPath: "./sidebars.ts",
           showLastUpdateAuthor: true,
           showLastUpdateTime: true,
-        },
-        // blog: {
-        //   showReadingTime: true,
-        //   // Please change this to your repo.
-        //   // Remove this to remove the "edit this page" links.
-        //   editUrl:
-        //     "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/"
-        // },
+        } satisfies DocsOptions,
+        blog: {
+          path: "blog",
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
+          showReadingTime: true,
+          postsPerPage: 5,
+          feedOptions: {
+            type: "all",
+            description:
+              "Keep up to date with FDM Monster and articles by following our feed!",
+            copyright: `Copyright © ${new Date().getFullYear()} David Zwart.`,
+            xslt: true,
+          },
+          editUrl: ({ blogDirPath, blogPath }) =>
+            `https://github.com/facebook/docusaurus/edit/main/website/${blogDirPath}/${blogPath}`,
+          blogTitle: "FDM Monster blog",
+          blogDescription: "Read blog posts about FDM Monster from the team",
+          blogSidebarCount: "ALL",
+          blogSidebarTitle: "All our posts",
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
+        } satisfies BlogOptions,
         theme: {
-          customCss: "./src/css/custom.css"
-        }
-      } satisfies Preset.Options
-    ]
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies PresetOptions,
+    ],
   ],
 
-  themeConfig:
-    {
-      algolia: {
-        // The application ID provided by Algolia
-        appId: "BZ7UWA7P64",
-        // Public API key: it is safe to commit it
-        apiKey: "58008360f3754a5ce748b4c13b855a64",
-        // Index to be searched on Algolia
-        indexName: "fdm-monster",
-        // Optional: see doc section below
-        contextualSearch: true,
-        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-        externalUrlRegex: "docs.fdm-monster\\.net|fdm-monster\\.net",
-        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
-        replaceSearchResultPathname: {
-          from: "/docs/", // or as RegExp: /\/docs\//
-          to: "/"
-        },
-        // Optional: Algolia search parameters (might require regenerating index at
-        // https://crawler.algolia.com/admin/crawlers/?sort=status&order=ASC&limit=20&appId=BZ7UWA7P64
-        // searchParameters: {
-        // },
-        // // Optional: path for search page that enabled by default (`false` to disable it)
-        searchPagePath: "search"
-        //... other Algolia params
-      },
-
-      // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
-      navbar: {
-        title: "FDM Monster",
-        logo: {
-          alt: "FDM Monster Logo",
-          src: "img/fdm-monster-logo.svg"
-        },
-        items: [
-          {
-            type: "docSidebar",
-            sidebarId: "tutorialSidebar",
-            position: "left",
-            label: "Getting Started"
-          },
-          // { to: "/blog", label: "Blog", position: "left" },
-          {
-            href: "https://github.com/fdm-monster/fdm-monster-docs",
-            label: "GitHub",
-            position: "right"
+  plugins: [
+    [
+      'client-redirects',
+      {
+        fromExtensions: ['html'],
+        createRedirects(routePath) {
+          // Redirect to /docs from /docs/introduction (now docs root doc)
+          if (routePath === '/docs/developer_setup' || routePath === '/docs/developer_setup/') {
+            return [`${routePath}/developer_setup/introduction`];
           }
-        ]
-      },
-      footer: {
-        style: "dark",
-        links: [
+          return [];
+        },
+        redirects: [
           {
-            title: "Docs",
-            items: [
-              {
-                label: "Getting Started",
-                to: "/docs/"
-              }
-            ]
+            from: ['/docs/ourteam'],
+            to: '/community/ourteam',
           },
-          {
-            title: "Community",
-            items: [
-              {
-                label: "Discord",
-                href: "https://discord.gg/mwA8uP8CMc"
-              },
-              {
-                label: "GitHub",
-                href: "https://github.com/fdm-monster/fdm-monster"
-              }
-            ]
-          },
-          {
-            title: "More",
-            items: [
-              {
-                label: "Site",
-                href: "https://fdm-monster.net/"
-              },
-              {
-                label: "Demo",
-                href: "https://demo.fdm-monster.net/"
-              }
-            ]
-          }
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} David Zwart. Built with <a href="https://github.com/facebook/docusaurus">Docusaurus</a>.`
+      } satisfies ClientRedirectsOptions,
+    ],
+  ],
+
+  themeConfig: {
+    algolia: {
+      // The application ID provided by Algolia
+      appId: "BZ7UWA7P64",
+      // Public API key: it is safe to commit it
+      apiKey: "58008360f3754a5ce748b4c13b855a64",
+      // Index to be searched on Algolia
+      indexName: "fdm-monster",
+      // Optional: see doc section below
+      contextualSearch: true,
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      externalUrlRegex: "docs.fdm-monster\\.net|fdm-monster\\.net",
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      replaceSearchResultPathname: {
+        from: "/docs/", // or as RegExp: /\/docs\//
+        to: "/",
       },
-      prism: {
-        colorMode: {
-          // Let the system decide with 'respectPrefersColorScheme' below
-          // defaultMode: 'dark',
-          respectPrefersColorScheme: true
+      // Optional: Algolia search parameters (might require regenerating index at
+      // https://crawler.algolia.com/admin/crawlers/?sort=status&order=ASC&limit=20&appId=BZ7UWA7P64
+      // searchParameters: {
+      // },
+      // // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: "search",
+      //... other Algolia params
+    },
+
+    // Replace with your project's social card
+    image: "img/docusaurus-social-card.jpg",
+    navbar: {
+      title: "FDM Monster",
+      logo: {
+        alt: "FDM Monster Logo",
+        src: "img/fdm-monster-logo.svg",
+      },
+      items: [
+        {
+          type: "doc",
+          position: "left",
+          docId: "introduction",
+          label: "Docs",
         },
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-        additionalLanguages: ["bash", "json", "powershell"]
-      }
-    }
+        {
+          type: 'docSidebar',
+          position: 'left',
+          sidebarId: 'community',
+          label: "Community",
+        },
+        {
+          to: "/blog",
+          label: "Blog",
+          position: "left"
+        },
+        {
+          type: 'docSidebar',
+          position: 'left',
+          sidebarId: 'development',
+          label: 'Development',
+        },
+        {
+          href: "https://github.com/fdm-monster/fdm-monster-docs",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Getting Started",
+              to: "/docs/",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Discord",
+              href: "https://discord.gg/mwA8uP8CMc",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/fdm-monster/fdm-monster",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Site",
+              href: "https://fdm-monster.net/",
+            },
+            {
+              label: "Demo (latest release)",
+              href: "https://demo.fdm-monster.net/",
+            },
+            {
+              label: "Test (bleeding edge release)",
+              href: "https://test.demo.fdm-monster.net/",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} David Zwart. Built with <a href="https://github.com/facebook/docusaurus">Docusaurus</a>.`,
+    },
+    prism: {
+      colorMode: {
+        // Let the system decide with 'respectPrefersColorScheme' below
+        // defaultMode: 'dark',
+        respectPrefersColorScheme: true,
+      },
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+      additionalLanguages: ["bash", "json", "powershell"],
+    },
+  },
 };
 
 export default config;
