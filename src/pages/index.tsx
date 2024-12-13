8abf2938ce5f685f621be6bbc4813990133335d8
davidzwa <davidzwa@users.noreply.github.com>
2024-12-13 14:53:49 +0100

    Update src/pages/index.tsx
    Co-authored-by: Maurice Kevenaar <maurice@kevenaar.name>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,7 +37,7 @@ export default function Home() {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="The documentation site for FDM Monster"
+      description="FDM Monster documentation - Are you ready to scale up your 3D Print farm?"
     >
       <HomepageHeader />
       <main>
